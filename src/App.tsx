a2b97ce4ea4018968ba75d0a9c6f2b33f90aff5a
Michael Schlueter <schluet0r4life@gmail.com>
2022-01-17 18:41:19 +0100

    added header with styling

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 
 function App() {
   return (
-    <h1 className='text-red-500'>Hello World</h1>
+    <div className='container border-2 m-auto'>
+      <header className='flex justify-between items-center my-10'>
+        <h1 className='font-semibold text-4xl'>Budgets</h1>
+        <div>
+        <button className='bg-blue-500 hover:bg-blue-700 text-white mr-2 py-2 px-3 rounded'>Add Budget</button>
+        <button className='bg-transparent hover:bg-blue-500 text-blue-700 hover:text-white py-2 px-2 border border-blue-500 hover:border-transparent rounded'>Add Expense</button>
+        </div>
+      </header>
+    </div>
+
+
   );
 }
 
