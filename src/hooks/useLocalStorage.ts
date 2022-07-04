f32e10ff8c612ce9d6396523806efc1dbbd3ef77
Michael Schlueter <schluet0r4life@gmail.com>
2022-07-04 16:10:39 +0200

    update types for useLocalStorage hook

--- src/hooks/useLocalStorage.ts
+++ src/hooks/useLocalStorage.ts
@@ -1,27 +1,25 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
-export default function useLocalStorage(
-  key: string,
-  defaultValue: Function | []
-) {
-  const [value, setValue] = useState(() => {
-    // getting value from local storage
+export function useLocalStorage<T>(key: string, initialValue: T | (() => T)) {
+  const [value, setValue] = useState<T>(() => {
+        // getting value from local storage
     const jsonValue = localStorage.getItem(key);
-    // checking if value is existing in local storage, then return it
+        // checking if value is existing in local storage, then return it
     if (jsonValue != null) return JSON.parse(jsonValue);
 
-    // if value is not existing, return default value
-    if (typeof defaultValue === "function") {
-      return defaultValue();
+        // if value is not existing, return default value
+    if (typeof initialValue === "function") {
+      return (initialValue as () => T)();
     } else {
-      return defaultValue;
+      return initialValue;
     }
   });
 
   // updating local storage whenever key or value change
   useEffect(() => {
-    localStorage.setItem(key, JSON.stringify(value));
-  }, [key, value]);
+    localStorage.setItem(key, JSON.stringify(value))
+  }, [key, value])
 
-  return [value, setValue];
+  return [value, setValue] as [typeof value, typeof setValue]
 }
+
